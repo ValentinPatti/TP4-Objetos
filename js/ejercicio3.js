6208139197509c4c3cd9ad39fdf7969c76a35974
Valentin Patti <gonzalezvalentin62c1@gmail.com>
2026-03-31 22:34:04 -0300

    Ejercicio 3 resuelto

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,16 +1,35 @@
-//Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
+//Escribe un programa para que permita crear objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
 
-const rectangulo = {
-    alto: 0,
-    ancho: 0,
+function crearRectangulo(alto, ancho) {
+  return {
+    alto: alto, //la primera es la propiedad y la segunda es el valor del parametro
+    ancho: ancho,
 
-    alto(altoIngresado){
-        alto += altoIngresado
+    // setAlto: function (nuevoAlto) {
+    //   this.alto = nuevoAlto;
+    // },
+    // setAncho: function (nuevoAncho) {
+    //   this.ancho = nuevoAncho;
+    // },
+
+    mostrarPropiedades: function () {
+      console.log(
+        `Dimensiones: ${this.alto} de alto y ${this.ancho} de ancho.`,
+      );
     },
-    ancho(anchoIngresado){
-        this.ancho += anchoIngresado
-    }
+    calcularArea: function () {
+      return this.alto * this.ancho;
+    },
+    calcularPerimetro: function () {
+      return 2 * (this.alto + this.ancho);
+    },
+  };
 }
 
-rectangulo.alto()
-rectangulo.ancho()
+const miRectangulo = crearRectangulo(5, 8);
+
+miRectangulo.mostrarPropiedades();
+console.log("Área:", miRectangulo.calcularArea());
+
+miRectangulo.setAlto();
+console.log("Nuevo Perímetro:", miRectangulo.calcularPerimetro());
